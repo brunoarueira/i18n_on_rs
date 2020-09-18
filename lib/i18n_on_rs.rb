3ff2a8975f936add3a69986d1740ccb18c5a7948
Bruno Arueira <contato@brunoarueira.com>
2020-09-18 19:40:41 -0300

    Initial commit

---
+++ lib/i18n_on_rs.rb
@@ -0,0 +1,6 @@
+require "i18n_on_rs/version"
+
+module I18nOnRs
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/i18n_on_rs_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe I18nOnRs do
+  it "has a version number" do
+    expect(I18nOnRs::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
